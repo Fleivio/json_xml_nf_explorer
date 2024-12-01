356d1042f304048023cb3c8810de1c31a287fc98
Anomalocaris <flavioborinjunior@gmail.com>
2024-12-01 19:08:58 -0300

    Schema funciona para todas as nfs (ainda com apenas 1 produto)

--- src/MainSchema.hs
+++ src/MainSchema.hs
@@ -1,19 +1,20 @@
-module MainSchema(main') where 
+module MainSchema(testSchema) where 
 
 import Data.Aeson (Value, eitherDecode, encode, decode)
 import qualified Data.ByteString.Lazy as B
 import Data.Aeson.Schema
-import Data.Maybe (fromJust)
 import Schemas.RootSchema
 import Schemas.HeaderSchema
 
-main' :: IO ()
-main' = do
-    str <- B.readFile "files/test.json"
+default(String)
+
+testSchema :: FilePath -> IO ()
+testSchema path = do
+    str <- B.readFile ("files/" ++ path)
     let d = eitherDecode str :: (Either String Value)
     case d of
         Left err -> putStrLn err
-        Right ps -> print $ mkTest ps
+        Right _ -> print "Success"
 
 mkTest :: Value -> Maybe (Object MySchema)
 mkTest = decode . encode
