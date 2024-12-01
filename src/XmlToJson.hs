2196490adae20c0f0ee004058d8675d3a5ae44b1
Anomalocaris <flavioborinjunior@gmail.com>
2024-12-01 12:33:21 -0300

    transformação pronta xml->json

--- src/XmlToJson.hs
+++ src/XmlToJson.hs
@@ -8,12 +8,16 @@ import Data.Aeson.Key
 import qualified Data.ByteString.Lazy as B
 
 data Tag = Tag String [(String, String)] (Either [Tag] String) deriving(Show, Eq, Generic)
+data Xml = Xml [(String, String)] Tag deriving(Show, Eq, Generic)
 
 instance ToJSON Tag where
-  toJSON (Tag name attrs inner) = object [fromString name .= object ["_attrs" .= attrs, "_content" .= content]]
+  toJSON (Tag name attrs inner) =
+      object [fromString name .= object ["_attrs" .= attrs, "_content" .= content]]
     where content = case inner of
                       Left tags -> toJSON tags
                       Right str -> toJSON str
+
+instance ToJSON Xml
 
 transformFile :: FilePath -> FilePath -> IO ()
 transformFile input output = do
@@ -25,37 +29,54 @@ transformFile input output = do
     Right tag -> let jsonBs = encode tag
                  in B.writeFile output jsonBs
 
-parseXML :: String -> Either ParseError Tag
-parseXML = parse tagParser ""
+parseXML :: String -> Either ParseError Xml
+parseXML = parse xmlParser ""
+
+xmlParser :: Parser Xml
+xmlParser = do
+  ignores
+  header <- headerParser
+  tag <- tagParser
+  eof
+  return $ Xml header tag
+
+headerParser :: Parser [(String,String)]
+headerParser = do
+  string "<?xml"
+  attrs <- many attrParser
+  string "?>"
+  return attrs
 
 ignores :: Parser String
 ignores = many $ oneOf " \n\t"
 
 tagParser :: Parser Tag
-tagParser = do
-  ignores
-  (name, attrs) <- openTag
-  inner <- innerParser
-  closeTag name
-  ignores
-  return $ Tag name attrs inner
-
+tagParser = 
+  try emptyTag <|> contentTag
+  where 
+    emptyTag = do
+      ignores
+      char '<'
+      ignores
+      name <- many1 $ noneOf " \n\t/>"
+      attrs <- many attrParser
+      string "/>"
+      ignores
+      return $ Tag name attrs (Right "")
+    contentTag = do 
+      ignores
+      (name, attrs) <- openTag 
+      inner <- innerParser
+      closeTag name
+      ignores
+      return $ Tag name attrs inner
+  
 openTag :: Parser (String, [(String, String)])
 openTag = do
   char '<'
   ignores
   name <- many1 $ noneOf " \n\t/>"
-  attrs <- many $ do
-    ignores
-    attrName <- many1 $ noneOf " \n\t=>"
-    ignores
-    char '='
-    ignores
-    char '"'
-    attrValue <- many1 $ noneOf "\""
-    char '"'
-    ignores
-    return (attrName, attrValue)
+  attrs <- many attrParser
   char '>'
   return (name, attrs)
 
@@ -68,11 +89,24 @@ closeTag name = do
   char '>'
   return ()
 
+attrParser :: Parser (String, String)
+attrParser = do
+  ignores
+  name <- many1 $ noneOf " \n\t=?/>"
+  ignores
+  char '='
+  ignores
+  char '"'
+  value <- many1 $ noneOf "\""
+  char '"'
+  ignores
+  return (name, value)
+
 innerParser :: Parser (Either [Tag] String)
 innerParser = (Left <$> bodyParser) <|> (Right <$> pcdataParser)
 
 bodyParser :: Parser [Tag]
-bodyParser = many1 (try tagParser)
+bodyParser = many1 (try tagParser) 
 
 pcdataParser :: Parser String
 pcdataParser = many (noneOf "<")
